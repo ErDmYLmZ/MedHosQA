88a53892d7142867b2217f0f4192067c33a55b6e
ErDmYLmZ <erdemyilmaz4@gmail.com>
2021-12-29 22:10:37 +0100

    29DecFirstCommit

---
+++ src/test/java/stepdefinitions/seleniumuisteps/US08_PasswordSteps.java
@@ -0,0 +1,49 @@
+package stepdefinitions.seleniumuisteps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.RegistrationPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class US08_PasswordSteps {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Given("user is on the registration page")
+    public void user_is_on_the_registration_page() {
+        Driver.getDriver().get(ConfigReader.getProperty("registerAppUrl"));
+    }
+    @When("user enters the password as {string}")
+    public void user_enters_the_password_as(String firstPassword) {
+        registrationPage.firstPasswordTextbox.sendKeys(firstPassword);
+
+    }
+    @When("user confirms the new password {string}")
+    public void user_confirms_the_new_password(String secondPassword) {
+        registrationPage.newPasswordTextbox.sendKeys(secondPassword);
+        ReusableMethods.waitFor(1);
+        registrationPage.firstPasswordTextbox.click();
+
+    }
+
+    @And("Verify error message")
+    public void verifyNoErrorMessage() {
+       // switch ("")
+        String attributeOfPasswordBox=registrationPage.newPasswordTextbox.getAttribute("class");
+        Assert.assertTrue(attributeOfPasswordBox.contains("invalid"));
+        System.out.println(attributeOfPasswordBox);
+        //Assert.assertTrue(registrationPage.passwordErrorMessage.isDisplayed());
+//        if (registrationPage.passwordErrorMessage.isDisplayed()){
+//            String errorMessage=registrationPage.passwordErrorMessage.getText();
+//            Assert.assertEquals("The password and its confirmation do not match!", errorMessage);
+//        }else{
+//            Assert.assertFalse(registrationPage.passwordErrorMessage.isDisplayed());
+//        }
+
+
+
+    }
+}
---
+++ src/test/resources/features/UIFeatures/us08_Password.feature
@@ -0,0 +1,12 @@
+Feature: US_008 Password segment on Homepage should be editable
+  Background: Navigate to the Homepage
+    Given user is on the registration page
+
+    Scenario Outline: Verify password confirmation
+    When user enters the password as "<firstpassword>"
+    And user confirms the new password "<secondpassword>"
+    And Verify error message
+      Examples:
+      |firstpassword|secondpassword|
+      |Ab12345.     |Ab12345.      |
+      |Ab12345.     |Ab12345       |
